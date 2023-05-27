f49633e3602d22bf8e6fad6fdd9e646be05b1b13
jack <jrhaben@gmail.com>
2023-05-27 07:02:28 -0500

    break point

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,44 +6,13 @@
       <div class="text-body-2 font-weight-light mb-n1"> </div>
 
       
-      <h1 class="text-h3 font-weight-bold my-1">
+      <h1 class="title  font-weight-bold my-1">
         <span style="color: #07689F;">ROCA</span>
         <span style="color: #A2D5F2;">SOLIDA</span>
         <span style="color: #07689F;">.TECH</span>
       </h1>
       <h2 class="pulse">Coming Soon</h2>
 
-
-   
-
-      <!-- <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn href="https://vuetifyjs.com/components/all/" min-width="164" rel="noopener noreferrer" target="_blank"
-            variant="text">
-            <v-icon icon="mdi-view-dashboard" size="large" start />
-
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn color="primary" href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides" min-width="228"
-            rel="noopener noreferrer" size="x-large" target="_blank" variant="flat">
-            <v-icon icon="mdi-speedometer" size="large" start />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn href="https://community.vuetifyjs.com/" min-width="164" rel="noopener noreferrer" target="_blank"
-            variant="text">
-            <v-icon icon="mdi-account-group" size="large" start />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row> -->
     </v-responsive>
   </v-container>
 </template>
@@ -69,5 +38,17 @@
 
 .pulse {
   animation: pulse 2s infinite;
+}
+
+
+
+.title {
+  font-size: 3.5em;
+}
+
+@media (max-width: 600px) {
+  .title {
+    font-size: 2em;
+  }
 }
 </style>
