a23bc90c0a59ecfe35d0158f217b9cc38aa3b88f
jack <jrhaben@gmail.com>
2023-05-27 06:53:33 -0500

    pulse coming soon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,17 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class=" align-center text-center fill-height">
-      <v-img height="92" src="@/assets/logo.png" />
+      <v-img height="300" src="@/assets/logo.png" />
      
       <div class="text-body-2 font-weight-light mb-n1"> </div>
 
       
       <h1 class="text-h3 font-weight-bold my-1">
-        <span style="color: #07689F;">DURA</span>
-        <span style="color: #A2D5F2;">DERO</span>
+        <span style="color: #07689F;">ROCA</span>
+        <span style="color: #A2D5F2;">SOLIDA</span>
         <span style="color: #07689F;">.TECH</span>
       </h1>
-      <h2>Coming Soon</h2>
+      <h2 class="pulse">Coming Soon</h2>
 
 
    
@@ -51,3 +51,23 @@
 <script setup>
   //
 </script>
+<style scoped>
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  50% {
+    transform: scale(1.1);
+    opacity: 0.5;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+
+.pulse {
+  animation: pulse 2s infinite;
+}
+</style>
