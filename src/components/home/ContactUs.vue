3d4110b9c94580e186468846bcc9f5801ea384d2
jack <jrhaben@gmail.com>
2023-06-08 10:26:07 -0500

    add fb wa icon

--- src/components/home/ContactUs.vue
+++ src/components/home/ContactUs.vue
@@ -6,8 +6,21 @@
         <v-card-title>{{
           spanishStatus
         }}</v-card-title>
-        <v-btn @click="sendEmail"><v-icon color="primary" size="xx-large">mdi-email</v-icon></v-btn>
+        <v-btn class="mt-2" @click="sendEmail"><v-icon color="primary" size="xx-large">mdi-email</v-icon></v-btn>
         <v-card-title> info@rocasolida.tech </v-card-title>
+        <v-row justify="center" class="ma-4">
+
+          <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4"
+            style="font-size: 20px; background-color: white" rounded size="x-large" target="_blank"
+            href="https://www.facebook.com/RocaSolidaTech">
+            <v-icon color="primary" size="50">
+              {{ "mdi-facebook" }}</v-icon></v-btn>
+              <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4"
+            style="font-size: 20px; background-color: white" rounded size="x-large" target="_blank"
+            href="https://wa.me/6514421267">
+            <v-icon color="primary" size="50">
+              {{ "mdi-whatsapp" }}</v-icon></v-btn>
+        </v-row>
         <v-card-title> St. Paul, MN </v-card-title>
       </v-card-item>
     </v-card>
@@ -30,11 +43,6 @@ const spanish = ref("Visítanos")
 const spanishStatus = computed(() => languageStore.getLanguage ? spanish.value : english.value)
 </script>
 
-
-
-
 <style scoped>
 /* Add your styles here */
 </style>
-
-  
