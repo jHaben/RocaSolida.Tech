ddaf5d231c6a4d33990cdf12d08304d57dcb6cac
jack <jrhaben@gmail.com>
2023-06-09 12:18:59 -0500

    twitter

--- src/components/home/ContactUs.vue
+++ src/components/home/ContactUs.vue
@@ -9,17 +9,22 @@
         <v-btn class="mt-2" @click="sendEmail"><v-icon color="primary" size="xx-large">mdi-email</v-icon></v-btn>
         <v-card-title> info@rocasolida.tech </v-card-title>
         <v-row justify="center" class="ma-4">
-
           <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4"
             style="font-size: 20px; background-color: white" rounded size="x-large" target="_blank"
-            href="https://www.facebook.com/RocaSolidaTech">
+            href="https://twitter.com/rocaSolidaTech">
             <v-icon color="primary" size="50">
-              {{ "mdi-facebook" }}</v-icon></v-btn>
+              {{ "mdi-twitter" }}</v-icon></v-btn>
+
               <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4"
             style="font-size: 20px; background-color: white" rounded size="x-large" target="_blank"
             href="https://wa.me/6514421267">
             <v-icon color="primary" size="50">
               {{ "mdi-whatsapp" }}</v-icon></v-btn>
+              <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4"
+            style="font-size: 20px; background-color: white" rounded size="x-large" target="_blank"
+            href="https://www.facebook.com/RocaSolidaTech">
+            <v-icon color="primary" size="50">
+              {{ "mdi-facebook" }}</v-icon></v-btn>
         </v-row>
         <v-card-title> St. Paul, MN </v-card-title>
       </v-card-item>
