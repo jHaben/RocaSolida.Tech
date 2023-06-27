cfa2be7c7de83b720d190d8cc88658f2e3ab30bb
jack <jrhaben@gmail.com>
2023-06-27 13:14:39 -0500

    video

--- src/components/home/ContactUs.vue
+++ src/components/home/ContactUs.vue
@@ -9,25 +9,24 @@
         <v-btn class="mt-2" @click="sendEmail"><v-icon color="primary" size="xx-large">mdi-email</v-icon></v-btn>
         <v-card-title> info@rocasolida.tech </v-card-title>
         <v-row justify="center" class="ma-4">
-          <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4"
-            style="font-size: 20px; background-color: white" rounded size="x-large" target="_blank"
-            href="https://twitter.com/rocaSolidaTech">
+          <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4" style="font-size: 20px; background-color: white"
+            rounded size="x-large" target="_blank" href="https://twitter.com/rocaSolidaTech">
             <v-icon color="primary" size="50">
               {{ "mdi-twitter" }}</v-icon></v-btn>
 
-              <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4"
-            style="font-size: 20px; background-color: white" rounded size="x-large" target="_blank"
-            href="https://wa.me/6514421267">
+          <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4" style="font-size: 20px; background-color: white"
+            rounded size="x-large" target="_blank" href="https://wa.me/6514421267">
             <v-icon color="primary" size="50">
               {{ "mdi-whatsapp" }}</v-icon></v-btn>
-              <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4"
-            style="font-size: 20px; background-color: white" rounded size="x-large" target="_blank"
-            href="https://www.facebook.com/RocaSolidaTech">
+          <v-btn elevation="2" icon outlined class="emailBtn ma-2 text4" style="font-size: 20px; background-color: white"
+            rounded size="x-large" target="_blank" href="https://www.facebook.com/RocaSolidaTech">
             <v-icon color="primary" size="50">
               {{ "mdi-facebook" }}</v-icon></v-btn>
         </v-row>
         <v-card-title> St. Paul, MN </v-card-title>
       </v-card-item>
+
+      <div id="rumble_v2u206c"></div>
     </v-card>
   </div>
 </template>
@@ -46,7 +45,14 @@ const english = ref("Stay in Touch")
 const spanish = ref("Visítanos")
 
 const spanishStatus = computed(() => languageStore.getLanguage ? spanish.value : english.value)
+
+!function (r, u, m, b, l, e) { r._Rumble = b, r[b] || (r[b] = function () { (r[b]._ = r[b]._ || []).push(arguments); if (r[b]._.length == 1) { l = u.createElement(m), e = u.getElementsByTagName(m)[0], l.async = 1, l.src = "https://rumble.com/embedJS/u28fvq4" + (arguments[1].video ? '.' + arguments[1].video : '') + "/?url=" + encodeURIComponent(location.href) + "&args=" + encodeURIComponent(JSON.stringify([].slice.apply(arguments))), e.parentNode.insertBefore(l, e) } }) }(window, document, "script", "Rumble");
+
+
+Rumble("play", { "video": "v2u206c", "div": "rumble_v2u206c" });
 </script>
+
+
 
 <style scoped>
 /* Add your styles here */
